6b6d5b786f4b0a416895a3e4dea66f1414a9a62a
artemmakarevich1997 <artemmakarevich1997@gmail.com>
2021-04-02 15:30:26 +0300

    Added start point of the project

---
+++ src/main/java/ru/realityfamily/pandorabackend/PandorabackendApplication.java
@@ -0,0 +1,13 @@
+package ru.realityfamily.pandorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandorabackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandorabackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/realityfamily/pandorabackend/PandorabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.realityfamily.pandorabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandorabackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
