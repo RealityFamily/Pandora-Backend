8eda29471e38de2ed0e1b0e509b461f7ece502cb
artemmad <artemmakarevich1997@gmail.com>
2021-07-03 20:55:22 +0300

    Redirect to base page if registrationConfirmed is true

--- src/main/java/ru/realityfamily/pandorabackend/mvc/userregistration/controller/UserRegistrationController.java
+++ src/main/java/ru/realityfamily/pandorabackend/mvc/userregistration/controller/UserRegistrationController.java
@@ -97,6 +97,6 @@ public class UserRegistrationController {
 
         user.setEnabled(true);
         userClientService.saveRegistredUser(user);
-        return "redirect:/login.html";
+        return "redirect:/";
     }
 }
