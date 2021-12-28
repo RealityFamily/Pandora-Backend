4cede555bbe7340b5450e1b50fb649ab8f322bba
artemmad <artemmakarevich1997@gmail.com>
2021-12-28 23:39:10 +0300

    Fixed email adress

--- src/main/java/ru/realityfamily/pandorabackend/mvc/userregistration/listener/RegistrationListener.java
+++ src/main/java/ru/realityfamily/pandorabackend/mvc/userregistration/listener/RegistrationListener.java
@@ -1,8 +1,12 @@
 package ru.realityfamily.pandorabackend.mvc.userregistration.listener;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -13,34 +17,39 @@ import ru.realityfamily.pandorabackend.user.v1.service.IUserClientService;
 import java.util.UUID;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
+@PropertySource("classpath:application.properties")
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
 
-    private IUserClientService service;
+    private final IUserClientService service;
 
-    private MessageSource messages;
+    private final MessageSource messages;
 
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
+
+    @Setter
+    @Value("${server.web.address}")
+    private String baseServerAddress;
 
     @Override
     public void onApplicationEvent(OnRegistrationCompleteEvent event) {
         confirmRegistration(event);
     }
 
-    private void confirmRegistration(OnRegistrationCompleteEvent event){
+    private void confirmRegistration(OnRegistrationCompleteEvent event) {
         User user = event.getUser();
         String token = UUID.randomUUID().toString();
         service.createVerificationToken(user, token);
 
         String recipientAddress = user.getMail();
         String subject = "Подтверждение регистрации";
-        String confirmationUrl = event.getAppUrl() + "/registrationConfirm?token="+token;
+        String confirmationUrl = event.getAppUrl() + "/registrationConfirm?token=" + token;
         String message = "Вы зарегистрировались успешно. Кликните на ссылку ниже для подтверждения";//messages.getMessage("message.regSucc", null, event.getLocale());
 
         SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(recipientAddress);
         email.setSubject(subject);
-        email.setText(message+ "\r\n" + "https://pandora.dev.realityfamily.ru"+ confirmationUrl);//TODO : Need to concatenate the server address from request, that not hardcoded
+        email.setText(message + "\r\n" + "" + baseServerAddress.trim() + confirmationUrl);
         mailSender.send(email);
     }
 }
